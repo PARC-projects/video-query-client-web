1568c746bbb0a29dbf48afa5692b43196917c627
chad-ramos <jedi_obi_wan_@hotmail.com>
2018-08-08 17:23:55 -0500

    Add pagination tests

--- src/app/components/pagination/pagination.component.spec.ts
+++ src/app/components/pagination/pagination.component.spec.ts
@@ -1,7 +1,9 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { PaginationComponent } from './pagination.component';
 import { FormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
+import { PaginationComponent } from './pagination.component';
+import { IPagination } from '../../models/pagination';
+
 
 describe('PaginationComponent', () => {
   let component: PaginationComponent;
@@ -18,10 +20,81 @@ describe('PaginationComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(PaginationComponent);
     component = fixture.componentInstance;
+
+    component.pagination = {
+      count: 10,
+      previousPage: 1,
+      currentPage: 2,
+      nextPage: 3,
+      lastPage: 4,
+    } as IPagination;
+
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('perPageSelected', () => {
+    it('should emit 20 when the .custom-select is changed.', () => {
+      spyOn(component.perPageSelected, 'emit');
+
+      fixture.debugElement.query(By.css('.custom-select')).nativeElement.value = '20';
+      fixture.debugElement.query(By.css('.custom-select')).nativeElement.dispatchEvent(new Event('change'));
+      fixture.detectChanges();
+
+      expect(component.perPageSelected.emit).toHaveBeenCalledWith('20');
+    });
+  });
+
+  describe('paginationClick', () => {
+    it('should emit when the .page-item is clicked.', () => {
+      spyOn(component.paginationClick, 'emit');
+
+      fixture.debugElement.query(By.css('.page-item')).nativeElement.click();
+
+      expect(component.paginationClick.emit).toHaveBeenCalled();
+    });
+
+    it('should emit 1 when the .previous-arrow is clicked.', () => {
+      spyOn(component.paginationClick, 'emit');
+
+      fixture.debugElement.query(By.css('.previous-arrow')).nativeElement.click();
+
+      expect(component.paginationClick.emit).toHaveBeenCalledWith(1);
+    });
+
+    it('should emit 3 when the .next-arrow is clicked.', () => {
+      spyOn(component.paginationClick, 'emit');
+
+      fixture.debugElement.query(By.css('.next-arrow')).nativeElement.click();
+
+      expect(component.paginationClick.emit).toHaveBeenCalledWith(3);
+    });
+
+    it('should emit 1 when the .previous is clicked.', () => {
+      spyOn(component.paginationClick, 'emit');
+
+      fixture.debugElement.query(By.css('.previous')).nativeElement.click();
+
+      expect(component.paginationClick.emit).toHaveBeenCalledWith(1);
+    });
+
+    it('should emit 3 when the .next is clicked.', () => {
+      spyOn(component.paginationClick, 'emit');
+
+      fixture.debugElement.query(By.css('.next')).nativeElement.click();
+
+      expect(component.paginationClick.emit).toHaveBeenCalledWith(3);
+    });
+
+    it('should emit 2 when the .current is clicked.', () => {
+      spyOn(component.paginationClick, 'emit');
+
+      fixture.debugElement.query(By.css('.current')).nativeElement.click();
+
+      expect(component.paginationClick.emit).toHaveBeenCalledWith(2);
+    });
   });
 });
