bfa424509d246385aad467fc6ad55067c1c1d671
chad-ramos <jedi_obi_wan_@hotmail.com>
2018-08-14 14:27:30 -0500

    Add reference time validator

---
+++ src/app/pages/new-query/reference-time.validator.spec.ts
@@ -0,0 +1,9 @@
+import { ReferenceTimeValidatorDirective } from './reference-time.validator';
+
+describe('ReferenceTimeValidatorDirective', () => {
+
+  it('should create an instance', () => {
+    const directive = new ReferenceTimeValidatorDirective();
+    expect(directive).toBeTruthy();
+  });
+});
