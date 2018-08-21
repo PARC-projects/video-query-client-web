45f116ad134bbacbe5c4c3db24c2523dea5b7847
chad-ramos <jedi_obi_wan_@hotmail.com>
2018-08-21 15:57:30 -0500

    Add new query spec

--- src/app/pages/new-query/new-query.component.spec.ts
+++ src/app/pages/new-query/new-query.component.spec.ts
@@ -1,25 +1,41 @@
-// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
-// import { NewQueryComponent } from './new-query.component';
+import { NewQueryComponent } from './new-query.component';
+import { ComponentsModule } from '../../components/components.module';
+import { SearchSetRepository } from '../../repositories/search-set.repository';
+import { QueryRepository } from '../../repositories/query.repository';
+import { AlertService } from '../../services/alert.service';
 
-// describe('NewQueryComponent', () => {
-//   let component: NewQueryComponent;
-//   let fixture: ComponentFixture<NewQueryComponent>;
+describe('NewQueryComponent', () => {
+  let component: NewQueryComponent;
+  let fixture: ComponentFixture<NewQueryComponent>;
 
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [ NewQueryComponent ]
-//     })
-//     .compileComponents();
-//   }));
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule,
+        FormsModule,
+        ComponentsModule
+      ],
+      declarations: [NewQueryComponent],
+      providers: [
+        AlertService,
+        QueryRepository,
+        SearchSetRepository
+      ]
+    })
+      .compileComponents();
+  }));
 
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(NewQueryComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+  beforeEach(() => {
+    fixture = TestBed.createComponent(NewQueryComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
