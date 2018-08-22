6f594677127b4c07df2fa0fcf684bb5a6aa3b0ed
chad-ramos <jedi_obi_wan_@hotmail.com>
2018-08-22 11:16:49 -0500

    Add search set add service spec

--- src/app/pages/admin/search-set-add/services/search-set-add.service.spec.ts
+++ src/app/pages/admin/search-set-add/services/search-set-add.service.spec.ts
@@ -1,15 +1,27 @@
-// import { TestBed, inject } from '@angular/core/testing';
+import { TestBed, inject } from '@angular/core/testing';
 
-// import { SearchSetAddService } from './search-set-add.service';
+import { SearchSetAddService } from './search-set-add.service';
+import { AlertService } from '../../../../services/alert.service';
+import { SearchSetRepository } from '../../../../repositories/search-set.repository';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { VideoRepository } from '../../../../repositories/video.repository';
 
-// describe('SearchSetAddService', () => {
-//   beforeEach(() => {
-//     TestBed.configureTestingModule({
-//       providers: [SearchSetAddService]
-//     });
-//   });
+describe('SearchSetAddService', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        SearchSetAddService,
+        AlertService,
+        SearchSetRepository,
+        VideoRepository
+      ],
+      imports: [
+        HttpClientTestingModule
+      ]
+    });
+  });
 
-//   it('should be created', inject([SearchSetAddService], (service: SearchSetAddService) => {
-//     expect(service).toBeTruthy();
-//   }));
-// });
+  it('should be created', inject([SearchSetAddService], (service: SearchSetAddService) => {
+    expect(service).toBeTruthy();
+  }));
+});
