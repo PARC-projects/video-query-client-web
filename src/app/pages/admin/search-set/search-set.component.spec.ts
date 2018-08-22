7d49057868a08b068afcebe1b7c21d7cd5b9466b
chad-ramos <jedi_obi_wan_@hotmail.com>
2018-08-22 11:39:14 -0500

    Add search spec

--- src/app/pages/admin/search-set/search-set.component.spec.ts
+++ src/app/pages/admin/search-set/search-set.component.spec.ts
@@ -1,25 +1,45 @@
-// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
 
-// import { SearchSetComponent } from './search-set.component';
+import { SearchSetComponent } from './search-set.component';
+import { ListNavComponent } from '../../../components/list-nav/list-nav.component';
+import { ModalComponent } from '../../../components/modal/modal.component';
+import { LoaderComponent } from '../../../components/loader/loader.component';
+import { PaginationComponent } from '../../../components/pagination/pagination.component';
+import { SearchSetRepository } from '../../../repositories/search-set.repository';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
-// describe('SearchSetComponent', () => {
-//   let component: SearchSetComponent;
-//   let fixture: ComponentFixture<SearchSetComponent>;
+describe('SearchSetComponent', () => {
+  let component: SearchSetComponent;
+  let fixture: ComponentFixture<SearchSetComponent>;
 
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [ SearchSetComponent ]
-//     })
-//     .compileComponents();
-//   }));
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        SearchSetComponent,
+        ListNavComponent,
+        ModalComponent,
+        LoaderComponent,
+        PaginationComponent
+      ],
+      imports: [
+        FormsModule,
+        HttpClientTestingModule
+      ],
+      providers: [
+        SearchSetRepository
+      ]
+    })
+      .compileComponents();
+  }));
 
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(SearchSetComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SearchSetComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
