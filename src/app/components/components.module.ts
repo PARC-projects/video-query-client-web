c93dee2ba9768e1d723c4396abd61bf1c71b64c0
chad-ramos <jedi_obi_wan_@hotmail.com>
2019-11-11 10:15:54 -0600

    Add baselayout for modal

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -14,6 +14,7 @@ import { FormsModule } from '@angular/forms';
 import { PaginationComponent } from './pagination/pagination.component';
 import { RouterModule } from '@angular/router';
 import { TokenAuthComponent } from './token-auth/token-auth.component';
+import { OnboardingModalComponent } from './onboarding-modal/onboarding-modal.component';
 
 @NgModule({
   declarations: [
@@ -25,7 +26,8 @@ import { TokenAuthComponent } from './token-auth/token-auth.component';
     ModalComponent,
     NavComponent,
     PaginationComponent,
-    TokenAuthComponent
+    TokenAuthComponent,
+    OnboardingModalComponent
   ],
   imports: [
     FormsModule,
@@ -43,7 +45,8 @@ import { TokenAuthComponent } from './token-auth/token-auth.component';
     ModalComponent,
     NavComponent,
     PaginationComponent,
-    TokenAuthComponent
+    TokenAuthComponent,
+    OnboardingModalComponent
   ]
 })
 export class ComponentsModule { }
---
+++ src/app/components/onboarding-modal/onboarding-modal.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+
+import { ModalComponent } from 'src/app/components/modal/modal.component';
+
+@Component({
+  selector: 'app-onboarding-modal',
+  templateUrl: './onboarding-modal.component.html',
+  styleUrls: ['./onboarding-modal.component.scss']
+})
+export class OnboardingModalComponent implements OnInit {
+
+  public currentPage = 1;
+
+  @ViewChild(ModalComponent, { static: true }) private modalComponent: ModalComponent;
+
+  ngOnInit() {
+    this.modalComponent.open();
+  }
+
+  nextPage() {
+    this.currentPage++;
+  }
+}
