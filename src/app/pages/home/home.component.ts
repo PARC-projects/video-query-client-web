9a1195156bd1ffe18d93aa0f599cfecc5d9f1913
chad-ramos <jedi_obi_wan_@hotmail.com>
2019-11-13 12:02:28 -0600

    Remove unused methods from home component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,37 +1,28 @@
 import { Component, OnInit } from '@angular/core';
-import { Subscription } from 'rxjs';
-import { AlertService, AlertType } from '../../services/alert.service';
-import { ExistingQueryService } from './services/existing-query.service';
-import { ExistingQueryMatchService } from './services/existing-query-match.service';
 import { QueryRepository } from 'src/app/repositories/query.repository';
-import { IQueryResponse, IQuery } from 'src/app/models/query.model';
+import { IQuery } from 'src/app/models/query.model';
 import { IPagination } from 'src/app/models/pagination';
 
 @Component({
   selector: 'app-existing-query-page',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
-  providers: [
-    ExistingQueryService,
-    ExistingQueryMatchService
-  ]
 })
 export class HomeComponent implements OnInit {
-  canvasLoading = false;
-
+  isLoading = false;
   queries = [] as IQuery[];
   pagination: IPagination;
   searchTerm: string;
-  perPage = 10;
 
   private timeout: any;
 
   constructor(
-    public existingQueryService: ExistingQueryService,
-    public matchService: ExistingQueryMatchService,
-    private alertService: AlertService,
     private queryRepository: QueryRepository
   ) { }
+
+  ngOnInit(): void {
+    this.getQueries();
+  }
 
   onSearch(search: string): void {
     clearTimeout(this.timeout);
@@ -40,69 +31,22 @@ export class HomeComponent implements OnInit {
     }, 500);
   }
 
-  ngOnInit(): void {
-    this.getQueries();
-  }
-
-
   onPerPageSelection(perPageSelectedValue: number): void {
-    this.pagination.count = perPageSelectedValue;
+    this.getQueries(this.pagination.currentPage, perPageSelectedValue);
   }
 
   onPaginationClick(pageClicked: number): void {
     this.getQueries(pageClicked);
   }
 
-  private getQueries(page?: number): Promise<void> {
-    return this.queryRepository.getAll(page, this.searchTerm, this.perPage)
-      .toPromise()
-      .then((resp: IQueryResponse) => {
-        this.queries = resp.results;
-        this.pagination = resp.pagination;
-      })
-      .catch(() => {
-      });
-  }
+  private async getQueries(page?: number, perPage = 10): Promise<void> {
+    this.isLoading = true;
 
-  /**
-   * Emitter: app-query-header
-   */
-  onRevisionSubmit(): void {
-    document.body.scrollTop = document.documentElement.scrollTop = 0;
-    const message = `"${this.existingQueryService.currentQuery.name}": has been submitted for revision. Check back soon for results.`;
-    this.canvasLoading = true;
-    this.matchService.submitRevision(this.existingQueryService.currentQuery.id)
-      .then(() => {
-        return this.existingQueryService.updateQueryNote();
-      })
-      .then(() => {
-        this.alertService.setAlert(message, AlertType.Success);
-        this.canvasLoading = false;
-      })
-      .catch(() => {
-        this.canvasLoading = false;
-      });
-  }
+    const resp = await this.queryRepository.getAll(page, this.searchTerm, perPage)
+      .toPromise();
 
-
-  /**
-   * Emitter: app-query-header
-   */
-  onFinalizeSubmit(): void {
-    if (confirm(`Are you sure you would like to send this query to be finalized?`)) {
-      this.canvasLoading = true;
-      this.matchService.submitRevision(this.existingQueryService.currentQuery.id)
-        .then(() => {
-          return this.existingQueryService.updateQueryStateToProcessFinalized();
-        })
-        .then(() => {
-          const message = `"${this.existingQueryService.currentQuery.name}": has been submitted to be finalized`;
-          this.alertService.setAlert(message, AlertType.Success);
-          this.canvasLoading = false;
-        })
-        .catch(() => {
-          this.canvasLoading = false;
-        });
-    }
+    this.queries = resp.results;
+    this.pagination = resp.pagination;
+    this.isLoading = false;
   }
 }
