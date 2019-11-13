fd39786f43e72a5c9ea97253e13ca998223ad700
chad-ramos <jedi_obi_wan_@hotmail.com>
2019-11-12 18:41:34 -0600

    Add color getters

--- src/app/pages/existing-query/components/query/query.component.ts
+++ src/app/pages/existing-query/components/query/query.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { IQuery } from 'src/app/models/query.model';
+import { IQuery, ProcessState } from 'src/app/models/query.model';
 
 @Component({
   selector: 'app-query',
@@ -9,8 +9,72 @@ import { IQuery } from 'src/app/models/query.model';
 export class QueryComponent implements OnInit {
   @Input() query: IQuery;
 
-  public buttonText = 'Review Matches';
-  public stateText = 'In-Progress';
+  get buttonText(): string {
+    switch (this.query.process_state) {
+      case ProcessState.Submitted:
+      case ProcessState.Revised:
+      case ProcessState.Processing:
+      case ProcessState.Processed:
+        return 'Review Matches';
+      case ProcessState.Error:
+        return 'Download Report';
+      case ProcessState.Finalized:
+      case ProcessState.ProcessFinalized:
+        return 'Download Report';
+
+    }
+  }
+
+  get stateText(): string {
+    switch (this.query.process_state) {
+      case ProcessState.Submitted:
+      case ProcessState.Revised:
+      case ProcessState.Processing:
+        return 'In Progress';
+      case ProcessState.Processed:
+        return 'Ready for Review';
+      case ProcessState.Error:
+        return 'Error';
+      case ProcessState.Finalized:
+        return 'In Progress';
+      case ProcessState.ProcessFinalized:
+        return 'Ready for Download';
+    }
+  }
+
+  get buttonColor(): string {
+    switch (this.query.process_state) {
+      case ProcessState.Submitted:
+      case ProcessState.Revised:
+      case ProcessState.Processing:
+        return 'btn-secondary btn-light-secondary disabled';
+      case ProcessState.Processed:
+        return 'btn-warning';
+      case ProcessState.Error:
+        return 'btn-danger';
+      case ProcessState.Finalized:
+        return 'btn-secondary btn-light-secondary disabled';
+      case ProcessState.ProcessFinalized:
+        return 'btn-success';
+    }
+  }
+
+  get stateColor(): string {
+    switch (this.query.process_state) {
+      case ProcessState.Submitted:
+      case ProcessState.Revised:
+      case ProcessState.Processing:
+        return 'text-secondary';
+      case ProcessState.Processed:
+        return 'text-warning';
+      case ProcessState.Error:
+        return 'text-danger';
+      case ProcessState.Finalized:
+        return 'text-secondary';
+      case ProcessState.ProcessFinalized:
+        return 'text-success';
+    }
+  }
 
   ngOnInit() {
   }
