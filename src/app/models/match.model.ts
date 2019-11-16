70886d06f4404310c47c8abc1f1ed2538c70a2d3
chad-ramos <jedi_obi_wan_@hotmail.com>
2019-11-16 08:23:56 -0600

    Add video hover state

--- src/app/models/match.model.ts
+++ src/app/models/match.model.ts
@@ -14,7 +14,7 @@ export interface IMatch extends IPagination {
   is_match: boolean;
   user_match?: boolean;
   query_id: number;
-  match_video_time_span: number;
+  match_video_time_span: string;
   reference_video_external_source: boolean;
   reference_start_time: string;
   match_video_name: string;
--- src/app/pages/query/query.component.ts
+++ src/app/pages/query/query.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChildren, QueryList, ElementRef } from '@angular/core';
 import { QueryService } from './services/query.service';
 import { QueryMatchService } from './services/query-match.service';
 import { ActivatedRoute } from '@angular/router';
+import { IMatch } from 'src/app/models/match.model';
 
 @Component({
   selector: 'app-query',
@@ -14,7 +15,11 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class QueryComponent implements OnInit {
 
+
+  @ViewChildren('videoPlayer') components: QueryList<ElementRef>;
+
   isLoading = false;
+
   constructor(private route: ActivatedRoute,
     public queryMatchService: QueryMatchService,
     public queryService: QueryService) { }
@@ -24,5 +29,25 @@ export class QueryComponent implements OnInit {
     await this.queryService.getCurrentQuery(Number(this.route.snapshot.paramMap.get('id')));
     await this.queryMatchService.getMatches(this.queryService.currentQuery.id);
     this.isLoading = false;
+  }
+
+  videoMouseOver(match: IMatch) {
+    this.components.forEach(element => {
+      const attributeId = Number(element.nativeElement.getAttribute('data-message-id'));
+      if (attributeId === match.id) {
+        element.nativeElement.play();
+      }
+    });
+  }
+
+  videoMouseLeave(match: IMatch) {
+    this.components.forEach(element => {
+      const attributeId = Number(element.nativeElement.getAttribute('data-message-id'));
+      console.log(attributeId + ' ' + match.id);
+      if (attributeId === match.id) {
+        element.nativeElement.pause();
+        element.nativeElement.currentTime = match.match_video_time_span.split(',')[0];
+      }
+    });
   }
 }
