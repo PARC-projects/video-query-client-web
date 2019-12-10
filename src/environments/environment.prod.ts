d7dae042a138b724bd8a9cd0a816b3f719dd80c8
fetorres <frank.home@sbcglobal.net>
2019-12-10 07:42:34 -0800

    tweak wording on query page

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,28 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+enum AuthenticationTypeEnum {
+  HeaderToken
+}
+
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:8000',
+  confirmQueryChart: 1,
+  mailToAddress: 'test@test.com',
+  fileStoreRoot: 'assets/videos/',
+  externalSource: {
+    root: 'https://elasticbeanstalk-us-west-1-326964612130.s3-us-west-1.amazonaws.com/',
+    authentication: {
+      type: AuthenticationTypeEnum.HeaderToken,
+      uiMessage: 'This video comes from the Acme System and requires a token to access. Please provide the token assigned to you below.',
+      header: {
+        name: 'Video-Query',
+        value: 'i-am-a-token'
+      },
+      authEndpoint: 'https://stac.vtti.vt.edu/api/collections'
+    }
+  }
+};
