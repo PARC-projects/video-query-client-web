c289685d34cc31ee91eb740b3fabf3d8828244ac
chad-ramos <jedi_obi_wan_@hotmail.com>
2020-01-03 10:33:02 -0600

    Clean onboarding page and add new query page

---
+++ src/app/pages/new-query-two/new-query-two.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-query-two',
+  templateUrl: './new-query-two.component.html',
+  styleUrls: ['./new-query-two.component.scss'],
+})
+export class NewQueryTwoComponent {
+}
--- src/app/pages/onboarding/onboarding.component.ts
+++ src/app/pages/onboarding/onboarding.component.ts
@@ -1,53 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { QueryRepository } from 'src/app/repositories/query.repository';
-import { IQuery } from 'src/app/models/query.model';
-import { IPagination } from 'src/app/models/pagination';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-existing-query-page',
+  selector: 'app-onboarding-page',
   templateUrl: './onboarding.component.html',
   styleUrls: ['./onboarding.component.scss'],
 })
-export class OnboardingComponent implements OnInit {
-  isLoading = false;
-  queries = [] as IQuery[];
-  pagination: IPagination;
-  searchTerm: string;
-
-  private timeout: any;
-
-  constructor(
-    private queryRepository: QueryRepository
-  ) { }
-
-  ngOnInit(): void {
-    this.getQueries();
-  }
-
-  onSearch(search: string): void {
-    clearTimeout(this.timeout);
-    this.timeout = setTimeout(() => {
-      this.searchTerm = search;
-      this.getQueries(1);
-    }, 500);
-  }
-
-  onPerPageSelection(perPageSelectedValue: number): void {
-    this.getQueries(this.pagination.currentPage, perPageSelectedValue);
-  }
-
-  onPaginationClick(pageClicked: number): void {
-    this.getQueries(pageClicked);
-  }
-
-  private async getQueries(page?: number, perPage = 10): Promise<void> {
-    this.isLoading = true;
-
-    const resp = await this.queryRepository.getAll(page, this.searchTerm, perPage)
-      .toPromise();
-
-    this.queries = resp.results;
-    this.pagination = resp.pagination;
-    this.isLoading = false;
-  }
+export class OnboardingComponent {
 }
