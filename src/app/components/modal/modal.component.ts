56878f75575be85cf1d4006a127088988c29e523
chad-ramos <jedi_obi_wan_@hotmail.com>
2020-03-09 18:11:18 -0500

    Add 2-way binding to modal

--- src/app/components/modal/modal.component.ts
+++ src/app/components/modal/modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-modal',
@@ -10,18 +10,22 @@ export class ModalComponent {
   @Input() widthByPercentage = '50%';
   @Input() blockOverlayClose = false;
   @Input() show = false;
+  @Output() showChange: EventEmitter<boolean> = new EventEmitter<boolean>();
 
   close(): void {
     this.show = false;
+    this.showChange.emit(this.show);
   }
 
   overlayClose(): void {
     if (!this.blockOverlayClose) {
       this.show = false;
+      this.showChange.emit(this.show);
     }
   }
 
   open(): void {
     this.show = true;
+    this.showChange.emit(this.show);
   }
 }
