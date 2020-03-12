3c594aa80f005552f9cb47e57871d182f932b28f
chad-ramos <jedi_obi_wan_@hotmail.com>
2020-03-12 17:04:58 -0500

    Add binding to second page

--- src/app/pages/video-collection/video-collection.component.ts
+++ src/app/pages/video-collection/video-collection.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { SearchSetRepository } from 'src/app/repositories/search-set.repository';
+import { Video } from 'src/app/models/video.model';
 
 @Component({
   selector: 'app-video-collection',
@@ -6,10 +9,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./video-collection.component.scss']
 })
 export class VideoCollectionComponent implements OnInit {
+  public videos = [] as Video[];
+  public loading = false;
 
-  constructor() { }
+  constructor(
+    private searchSetRepository: SearchSetRepository,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.loading = true;
+      this.searchSetRepository.getVideosInSearchSet(params.id)
+        .subscribe((resp: Video[]) => {
+          this.videos = resp;
+        }).add(() => {
+          this.loading = false;
+        });
+    });
   }
 
 }
