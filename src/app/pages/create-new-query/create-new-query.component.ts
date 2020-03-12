76487924df27cfc268408e3f1330fe0fb5574e72
chad-ramos <jedi_obi_wan_@hotmail.com>
2020-03-12 14:21:40 -0500

    Add full binding

--- src/app/pages/create-new-query/create-new-query.component.ts
+++ src/app/pages/create-new-query/create-new-query.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, AbstractControl } from '@angular/forms';
 import { ISearchSetResponse } from 'src/app/models/search-set.model';
 import { SearchSetRepository } from 'src/app/repositories/search-set.repository';
 import { QueryRepository } from 'src/app/repositories/query.repository';
@@ -16,22 +16,23 @@ export class CreateNewQueryComponent implements OnInit {
   public searchSets: ISearchSetResponse;
   public videos: Video[];
   public videoSrc: string;
+  public startingTime = '00:00:00';
 
   public form = new FormGroup({
     name: new FormControl(null, Validators.required),
-    reference_time: new FormControl(null, Validators.required),
+    reference_time: new FormControl(this.startingTime, Validators.required),
     max_matches_for_review: new FormControl(null, Validators.required),
     use_dynamic_target_adjustment: new FormControl(false, Validators.required),
     video: new FormControl(null, Validators.required),
     current_video_length: new FormControl(null, Validators.required),
     search_set_to_query: new FormControl(null, Validators.required),
     notes: new FormControl(null),
-    process_state: new FormControl(1, Validators.required),
-
-    reference_time_seconds: new FormControl(null),  // tracking
-    reference_time_hours: new FormControl(null),  // tracking
-    reference_time_minutes: new FormControl(null), // tracking
+    process_state: new FormControl(1, Validators.required)
   });
+
+  get reference_time(): AbstractControl {
+    return this.form.get('reference_time');
+  }
 
   @ViewChild('videoPlayer', { static: true }) videoPlayer: ElementRef<HTMLVideoElement>;
 
@@ -59,26 +60,37 @@ export class CreateNewQueryComponent implements OnInit {
       });
   }
 
+  onSelectCurrentTime() {
+    this.form.get('reference_time').setValue(this.getFormattedReferenceTime(this.videoPlayer.nativeElement.currentTime));
+  }
+
+  onSubmitForm() {
+    this.loading = true;
+    this.queryRepository.add(this.form.value)
+      .subscribe(() => {
+        // this.alertService.setAlert(
+        //   `"${this.newQueryService.form.name}": has been submitted. Check back soon for results.`,
+        //   AlertType.Success
+        // );
+        // document.body.scrollTop = document.documentElement.scrollTop = 0;
+      }).add(() => {
+        this.loading = false;
+      });
+  }
+
   onReferenceSelect() {
     this.loading = true;
-    // TODO: A bit of a code smell. Abstract the two intents of the service call.
+    this.form.get('reference_time').setValue(this.startingTime);
     const url = this.getVideoPathBasedOnIdAndSetAuthenticationState();
     this.videoSrc = url + '#t=' + this.form.get('reference_time').value;
-
-    // if (this.newQueryService.showAuthentication) {
-    //   return this.handleUnauthenticatedState();
-    // }
-
     this.setVideoElementSource();
   }
 
   private getVideoPathBasedOnIdAndSetAuthenticationState(): string {
     let url = '';
-    // this.showAuthentication = false;
     for (let i = 0; i < this.videos.length; i++) {
       const video = this.videos[i];
       if (video.id === this.form.get('video').value) {
-        // this.showAuthentication = !video.is_authenticated;
         url = video.full_path;
         break;
       }
@@ -86,23 +98,37 @@ export class CreateNewQueryComponent implements OnInit {
     return url;
   }
 
+
   private setVideoElementSource() {
-    // this.videoDisplayState = VideoDisplayStateEnum.Valid;
     this.videoPlayer.nativeElement.addEventListener('loadeddata', () => {
       this.loading = false;
       this.form.get('current_video_length').setValue(this.videoPlayer.nativeElement.duration);
     }, false);
     this.videoPlayer.nativeElement.load();
-    this.onSeek();
+    // TODO: Enable if we want reference_time to update based on seek.
+    // this.onSeek();
   }
 
   private onSeek() {
     this.videoPlayer.nativeElement.addEventListener('seeking', () => {
-      let time = this.videoPlayer.nativeElement.currentTime;
-      this.form.get('reference_time_hours').setValue(Math.floor(time / 3600));
-      time = time - this.form.get('reference_time_hours').value * 3600;
-      this.form.get('reference_time_minutes').setValue(Math.floor(time / 60));
-      this.form.get('reference_time_seconds').setValue(Math.floor(time - this.form.get('reference_time_minutes').value * 60));
+      console.log(this.videoPlayer.nativeElement.currentTime);
+      this.form.get('reference_time').setValue(this.getFormattedReferenceTime(this.videoPlayer.nativeElement.currentTime));
     }, false);
+  }
+
+  private getFormattedReferenceTime(timeInSeconds: number): string {
+    const hours = Math.floor(timeInSeconds / 3600);
+    timeInSeconds = timeInSeconds - hours * 3600;
+    const min = Math.floor(timeInSeconds / 60);
+    const seconds = Math.floor(timeInSeconds - min * 60);
+    return `${this.leftPad(hours)}:${this.leftPad(min)}:${this.leftPad(seconds)}`;
+  }
+
+  private leftPad(originalNumber: number): string {
+    let str = originalNumber.toString();
+    while (str.length < 2) {
+      str = '0' + str;
+    }
+    return str;
   }
 }
