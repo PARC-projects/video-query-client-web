0bd2850526077cc4405d6c69809b8b6b8876cc5f
chad-ramos <jedi_obi_wan_@hotmail.com>
2020-03-12 13:00:10 -0500

    Bind reference call

--- src/app/pages/create-new-query/create-new-query.component.ts
+++ src/app/pages/create-new-query/create-new-query.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { ISearchSetResponse } from 'src/app/models/search-set.model';
+import { SearchSetRepository } from 'src/app/repositories/search-set.repository';
+import { QueryRepository } from 'src/app/repositories/query.repository';
+import { Video } from 'src/app/models/video.model';
 
 @Component({
   selector: 'app-create-new-query',
@@ -7,20 +11,43 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
   styleUrls: ['./create-new-query.component.scss']
 })
 export class CreateNewQueryComponent implements OnInit {
+  public loading = false;
+  public searchSets: ISearchSetResponse;
+  public videos: Video[];
+
 
   public form = new FormGroup({
     name: new FormControl(null),
+    search_set_id: new FormControl(null, Validators.required),
     reference_time: new FormControl(null, Validators.required),
     reference_time_seconds: new FormControl(null, Validators.required),
     reference_time_hours: new FormControl(null, Validators.required),
     reference_time_minutes: new FormControl(null),
-    max_matches_for_review: new FormControl(null),
+    max_matches_for_review: new FormControl(null, Validators.required),
     use_dynamic_target_adjustment: new FormControl(null)
   });
 
-  constructor() { }
+  constructor(
+    private searchSetRepository: SearchSetRepository,
+    private queryRepository: QueryRepository
+  ) { }
 
   ngOnInit(): void {
+    this.loading = true;
+    this.searchSetRepository.getAll()
+      .subscribe((resp: ISearchSetResponse) => {
+        this.searchSets = resp;
+      }).add(() => {
+        this.loading = false;
+      });
   }
 
+  onSelectedSearchSet() {
+    this.searchSetRepository.getVideosInSearchSet(this.form.get('search_set_id').value)
+      .subscribe((resp: Video[]) => {
+        this.videos = resp;
+      }).add(() => {
+        this.loading = false;
+      });
+  }
 }
