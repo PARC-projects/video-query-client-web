7854b6972d035ef5004f188ffa42fe29e50c649a
chad-ramos <jedi_obi_wan_@hotmail.com>
2020-03-19 14:05:22 -0500

    Add videos

--- src/app/pages/video-collection-new/video-collection-new.component.ts
+++ src/app/pages/video-collection-new/video-collection-new.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { IVideoResponse, Video } from 'src/app/models/video.model';
+import { IPagination } from 'src/app/models/pagination';
+import { ISearchSet } from 'src/app/models/search-set.model';
+import { VideoRepository } from 'src/app/repositories/video.repository';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-video-collection-new',
@@ -7,9 +12,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VideoCollectionNewComponent implements OnInit {
   public loading = false;
-  constructor() { }
+
+  /**
+   * New Search Set
+   */
+  searchSet = {
+    videos: [], // Collection of keys
+    duration: 10
+  } as ISearchSet;
+
+  /**
+   * Current videos in selected search set
+   */
+  videosInSearchSet = [] as Video[];
+
+  /**
+   * All videos base on pagination and filters
+   */
+  videos: Video[];
+
+  paginationConfig: IPagination;
+  searchTerm: string;
+  perPage = 10;
+  selectedSearchSet: ISearchSet;
+  searchSets: ISearchSet[];
+  videoSrc = '';
+  showModal = false;
+
+  constructor(
+    private videoRepository: VideoRepository
+  ) { }
 
   ngOnInit(): void {
+    this.loading = true;
+    this.getVideos();
   }
 
+  onVideoClick(path: string) {
+    this.videoSrc = `${environment.fileStoreRoot}${path}`;
+    this.showModal = true;
+  }
+
+  private getVideos(page?: number) {
+    return this.videoRepository.getAll(page, this.searchTerm, this.perPage)
+      .subscribe((resp: IVideoResponse) => {
+        this.videos = resp.results;
+        this.paginationConfig = resp.pagination;
+      }).add(() => {
+        this.loading = false;
+      });
+  }
 }
